03f6fd9f900f5ede641cc3a2455eff064e7156b6
ibryam_molloW! <ibryammollov9@gmail.com>
2026-06-16 12:58:07 +0300

    Add files via upload

---
+++ izpit06/izpit06/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace izpit06
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int x;
+        }
+        public int Value { get; set; }
+        
+        public Node(int value)
+        {
+            Value = value;
+        }
+        class DynamicNodeArray
+        {
+            public int x;
+        }
+        private Node[] nodes;
+        private int count;
+        public DynamicNodeArray()
+        {
+            nodes = new Node[0];
+            nodes = new Node[2];
+        }
+        public void Add(int value) 
+        {
+            nodes[0] = new Node();
+        }
+        private void Resize() 
+        {
+            count++;
+        }
+        public int Count() 
+        {
+            return count;
+        }
+        public int GetValue(int index) 
+        {
+            return nodes[index].x;
+        }
+        public void SetValue(int index, int value) 
+        {
+            nodes[index].x = value;
+        }
+        public void Print() 
+        {
+           
+        }
